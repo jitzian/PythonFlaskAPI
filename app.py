f2a9e1a41c1acb34813ea56ba8fd83861582e007
raian <you@example.com>
2018-02-03 01:14:37 -0500

    Refactoring queries and updating ERD

--- app.py
+++ app.py
@@ -19,13 +19,22 @@ class Stores(Resource):
 		query = conn.execute("select * from TB_STORE")
 		return {'stores': [i[1] for i in query.cursor.fetchall()]}
 
-class Lanes(Resource):
+class CatLanes(Resource):
 	def get(self):
 		#Connect to databse
 		conn = e.connect()
 		#Perform query and return JSON data
 		query = conn.execute("select * from TB_LANE")
-		return {'lanes': [i[2] for i in query.cursor.fetchall()]}
+		return {'catLanes': [i[2] for i in query.cursor.fetchall()]}
+
+class CatProducts(Resource):
+	def get(self):
+		#Connect to databse
+		conn = e.connect()
+		#Perform query and return JSON data
+		query = conn.execute("select * from TB_PRODUCT")
+		# return {'catProducts': [i[2] for i in query.cursor.fetchall()]}
+		return {'catProducts': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 
 class Categories(Resource):
 	def get(self):
@@ -39,17 +48,33 @@ class Products(Resource):
 	def get(self):
 		#Connect to databse
 		conn = e.connect()
-		strQry = "SELECT distinct PROD.PRO_NAME, PROD.PRO_DESCRIPTION, STORE.STO_NAME, CATEGORY.CAT_NAME, LANE.LAN_NAME, LANE.LAN_NUM_NAME from TB_STORE AS STORE, TB_PRODUCT AS PROD, TB_LANE AS LANE, TB_CATEGORY AS CATEGORY WHERE PROD.PK_TB_PRODUCT = CATEGORY.FK_TB_PRODUCT AND LANE.PK_TB_LANE = PROD.FK_TB_LANE"
+
+		strQry = """SELECT
+						distinct PROD.PRO_NAME,
+						PROD.PRO_DESCRIPTION,
+						STORE.STO_NAME,
+						CATEGORY.CAT_NAME,
+						LANE.LAN_NAME,
+						LANE.LAN_NUM_NAME
+						from
+							TB_STORE AS STORE,
+							TB_PRODUCT AS PROD,
+							TB_LANE AS LANE,
+							TB_CATEGORY AS CATEGORY
+						WHERE
+							PROD.PK_TB_PRODUCT = CATEGORY.FK_TB_PRODUCT
+							AND LANE.PK_TB_LANE = PROD.FK_TB_LANE"""
 
 		#Perform query and return JSON data
 		query = conn.execute(strQry)
-		result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+		result = {'products': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 		return result
 
 api.add_resource(Stores, '/stores')
-api.add_resource(Lanes, '/lanes')
+api.add_resource(CatLanes, '/catLanes')
 api.add_resource(Categories, '/categories')
 api.add_resource(Products, '/products')
+api.add_resource(CatProducts, '/catProducts')
 
 if __name__ == '__main__':
     app.run(debug=True)
